8780727d5b2033359c508170784e131f444db10e
mincheol <homerjay@naver.com>
2022-06-12 02:46:23 +0900

    init

---
+++ Trafel/Controllers/LoadingViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoadingViewController.swift
+//  Trafel
+//
+//  Created by 申民鐡 on 2022/06/12.
+//
+
+import UIKit
+
+class LoadingViewController: UIViewController {
+    // MARK: - Properties
+    
+    // MARK: - Init
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    // MARK: - Configures
+
+
+}
