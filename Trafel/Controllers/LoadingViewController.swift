f442a1dbe20fb9ef5ae973cf02c2db22dfc7cdd2
mincheol <homerjay@naver.com>
2022-06-12 03:36:06 +0900

    segue 이동, identifier활용, sceneDelegate,

--- Trafel/Controllers/LoadingViewController.swift
+++ Trafel/Controllers/LoadingViewController.swift
@@ -8,16 +8,54 @@
 import UIKit
 
 class LoadingViewController: UIViewController {
+ 
     // MARK: - Properties
+    private var isUserLoggedIn = true
     
     // MARK: - Init
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        configures()
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        //DispatchQueue로 비동기인데 딜레이를 준다. 2초동안 뺑글이돌고
+        DispatchQueue.main.asyncAfter(deadline: .now()+2) {
+            //showInitialView를 실행시킨다.
+            self.showInitialView()
+        }
+    }
 
     // MARK: - Configures
+    
+    func configures() {
+        setupView()
+    }
+    
+    private func setupView() {
+        view.backgroundColor = .orange
+    }
+    
+    private func showInitialView(){
+         //user가 로그인 되어있으면 ==> main tab bar로 이동
+         //user가 로그인 안 되어있으면 ==> onboarding 화면으로 이동
+        
+        if isUserLoggedIn {
+            //MainTabBarController를 만들어준다.
+            let mainTabBarController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier:"MainTabBarViewController")
+            
+            if let sceneDelegate = view.window?.windowScene?.delegate as? SceneDelegate,
+                let window = sceneDelegate.window {
+                window.rootViewController = mainTabBarController
+            }
+            
+        }else {
+            //지정해둔 identifier를 이용해서 화면을보내준다.
+            performSegue(withIdentifier: "showOnboarding", sender: nil)
+        }
+         
+    }
 
 
 }
